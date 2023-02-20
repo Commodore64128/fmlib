0c3a5c37f2916b97675f43aaaaab97736c483b67
Scott <scott.hutter@gmail.com>
2023-02-20 13:56:18 -0600

    added 3 instruments

--- sample.c
+++ sample.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "fm.h"
 
-void main(void) {
+void play_instruments() {
 
-    printf("FM Demo");
-
-    
+    // set volume
     fm_setchannelvol(1, 0x00);
     fm_setchannelvol(2, 0x00);
     fm_setchannelvol(3, 0x00);
 
-    fm_setwaveform(1, 1);
-    fm_setwaveform(2, 1);
-    fm_setwaveform(3, 1);
+    printf(" Piano...\n");
+    fm_setinstrument(PIANO);
+    fm_playsound(1, 4, 0);
+    sleep(3);
 
-    fm_setadsr(1, 13,9,4,13);
-    fm_setadsr(2, 13,9,4,13);
-    fm_setadsr(3, 13,9,4,13);
+    printf(" Brass...\n");
+    fm_setinstrument(BRASS);
+    fm_playsound(1, 4, 0);
+    sleep(3);
 
+    printf(" Guitar...\n");
+    fm_setinstrument(GUITAR);
+    fm_playsound(1, 4, 0);
+    sleep(3);
+
+}
+
+void play_chord() {
+
+    fm_setchannelvol(1, 0x00);
+    fm_setchannelvol(2, 0x00);
+    fm_setchannelvol(3, 0x00);
+
+    fm_setinstrument(PIANO);
+
+    printf("\nMajor C Chord\n");
     fm_playsound(1, 4, 0);
     fm_playsound(2, 4, 4);
     fm_playsound(3, 3, 7);
+}
+
+void main(void) {
+
+    printf("%cFM Demo\n",0x93);
+    printf("Accessing the SFX Sound Expander\n");
+    printf("CC65 Library Demo v1.0\n\n");
+
+    play_instruments();
+    play_chord();
+
+    
 
 }
